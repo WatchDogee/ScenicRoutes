0c13ca57689ad4c92a04d2f3b982436b7f1070b8
WatchDogee <mairiszeps@gmail.com>
2025-05-04 18:21:27 +0300

    comit

--- create-files.sh
+++ create-files.sh
@@ -1,5 +1,65 @@
 #!/bin/bash
 
+# Create bootstrap/app.php file
+mkdir -p bootstrap
+cat > bootstrap/app.php << 'EOL'
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+|
+| This script returns the application instance. The instance is given to
+| the calling script so we can separate the building of the instances
+| from the actual running of the application and sending responses.
+|
+*/
+
+return $app;
+EOL
+
 # Create artisan file
 cat > artisan << 'EOL'
 #!/usr/bin/env php
@@ -30,57 +90,28 @@ mkdir -p public
 cat > public/index.php << 'EOL'
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
+// Define the application start time
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
+// Check for maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
+// Register the Composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
+// Bootstrap the application
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+// Run the application
+$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $request = \Illuminate\Http\Request::capture()
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
 EOL
@@ -126,11 +157,11 @@ try {
     $dbName = getenv('DB_DATABASE');
     $dbUser = getenv('DB_USERNAME');
     $dbPass = getenv('DB_PASSWORD');
-    
+
     $dsn = "{$dbConnection}:host={$dbHost};port={$dbPort};dbname={$dbName}";
     $pdo = new PDO($dsn, $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
     $response['checks']['database'] = [
         'status' => 'ok',
         'message' => 'Database connection successful'
@@ -174,7 +205,7 @@ $response['checks']['artisan'] = [
 
 if (!$artisanExists) {
     $response['status'] = 'error';
-    
+
     // List files in the root directory
     $rootFiles = scandir('/app');
     $response['checks']['root_files'] = $rootFiles;
