9f82b644e187c218af7e63040f396ca08811a363
WatchDogee <mairiszeps@gmail.com>
2025-05-10 22:49:12 +0300

    deployment fix -migration

--- database/migrations/2025_05_10_173139_create_collections_table.php
+++ database/migrations/2025_05_10_173139_create_collections_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('collections', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('collections')) {
+            Schema::create('collections', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
